50606362c771206c62b904a4d349c15ed6693f08
hebatallah10 <heba@sketch-tech.com>
2023-12-03 11:38:49 +0200

    test

---
+++ app/Http/Controllers/ContactUsFormController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Contact;
+
+
+class ContactUsFormController extends Controller {
+    // Create Contact Form
+    public function createForm(Request $request) {
+      return view('contact');
+    }
+    // Store Contact Form data
+    public function ContactUsForm(Request $request) {
+        // Form validation
+        $this->validate($request, [
+            'fullName' => 'required',
+            'email' => 'required|email',
+            'phone' => 'required|regex:/^([0-9\s\-\+\(\)]*)$/|min:10',
+            'company'=>'required',
+            'message' => 'required'
+         ]);
+        //  Store data in database
+        Contact::create($request->all());
+        // 
+        return back()->with('success', 'We have received your message and would like to thank you for writing to us.');
+    }
+}
